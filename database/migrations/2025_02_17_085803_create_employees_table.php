f36420dc31fc9c29e4f8ae1328c13a8cb4d3ed45
Prayoga <41438462+prayoga5070@users.noreply.github.com>
2025-02-17 21:33:48 +0700

    add branch

---
+++ database/migrations/2025_02_17_085803_create_employees_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('nik')->nullable();
+            $table->string('name');
+            $table->string('dept_id');
+            $table->string('job_level');
+            $table->datetime('birthday');
+            $table->string('phone');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employees');
+    }
+};
